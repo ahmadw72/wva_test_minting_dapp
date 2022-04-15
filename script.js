f1716d472ccc0685192b04716cf0af43aea029f4
mc-akil <>
2022-04-15 07:35:21 +0500

    Updates

---
+++ script.js
@@ -0,0 +1,14 @@
+// //get the form by its id
+// const form = document.getElementById("contact-form"); 
+
+// //1.
+// const formEvent = form.addEventListener("submit", (event) => {
+//   event.preventDefault();
+
+//   //2.
+//   let mail = new FormData(form);
+
+//   //3.
+//   sendMail(mail);
+// })
+// window.alert("Hello world");
